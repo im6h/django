ecf15954e13ba0421b72b5ef155081140c172897
tjjone98 <tzero1898@gmail.com>
2020-06-27 16:00:14 +0700

    test deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5cmykw_&1wyfbic)t#*ui$j2!6jz)8%)%y3%dy*y^%6444)=^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shrouded-badlands-39835.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddeWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,5 +119,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-# Create your views here.
-
 from .serializers import HeroSerializer, PowerSerializer
 from .models import Hero, Power
 
 
-class HeroViewSet(viewsets.ReadOnlyModelViewSet):
+class HeroViewSet(viewsets.ModelViewSet):
   queryset = Hero.objects.all()
   serializer_class = HeroSerializer
 
 
-class PowerViewSet(viewsets.ReadOnlyModelViewSet):
+class PowerViewSet(viewsets.ModelViewSet):
   queryset = Power.objects.all()
   serializer_class = PowerSerializer
